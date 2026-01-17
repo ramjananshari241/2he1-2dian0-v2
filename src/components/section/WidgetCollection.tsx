6a57622f2b39ca61bc18c9eb1940f532ff27aaf0
ramjananshari241 <ramjananshari241@gmail.com>
2026-01-17 16:18:54 +0800

    Update WidgetCollection.tsx

--- src/components/section/WidgetCollection.tsx
+++ src/components/section/WidgetCollection.tsx
@@ -11,8 +11,12 @@ export const WidgetCollection = ({
       className="mb-6 grid grid-cols-2 gap-4 md:gap-8 lg:gap-10"
       data-aos="fade-up"
     >
+      {/* 左侧：Profile 组件 (保持不变) */}
       <ProfileWidget data={widgets.profile} />
-      <StatsWidget data={widgets.stats} />
+      
+      {/* 右侧：Stats 组件 (改为接收 announcement 数据) */}
+      {/* ⚠️ 注意：确保你在 Notion 中有一篇文章的 Slug 设置为了 'announcement' (或者 'annoucement'，请检查你的拼写) */}
+      <StatsWidget data={widgets.announcement} />
     </div>
   )
 }
