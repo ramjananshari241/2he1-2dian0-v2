79d12696f6271683fab3e275e37b1534441bfa36
ramjananshari241 <ramjananshari241@gmail.com>
2026-02-10 18:51:15 +0800

    Update deploy.js

--- src/pages/api/admin/deploy.js
+++ src/pages/api/admin/deploy.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   // 🔴 请务必把下面的 URL 换成你刚才复制的那个！
-  const VERCEL_HOOK = 'https://api.vercel.com/v1/integrations/deploy/prj_VLi5jzJ66SohK987g8lev5SPgiCi/Zg1wvfNE9W';
+  const VERCEL_HOOK = 'https://api.vercel.com/v1/integrations/deploy/prj_eQ8BCdUHAgDhJZ0MmihwcapIbjqI/DX5jLV7iu8';
   
   try {
     await fetch(VERCEL_HOOK, { method: 'POST' });
